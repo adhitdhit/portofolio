ee779e636c361ac13450746b13d94ef820d091ef
adhitdhit <141433718+adhitdhit@users.noreply.github.com>
2025-05-06 17:19:19 +0700

    Delete portofolio directory

--- portofolio/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('portofolio/', views.porto, name='porto'),
-]
--- portofolio/views.py
+++
@@ -1,7 +0,0 @@
-
-# Create your views here.
-from django.shortcuts import render
-
-def porto(request):
-    return render(request, 'index.html')
-
